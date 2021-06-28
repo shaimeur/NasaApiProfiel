14fd66d1821c5724acb9e43826247e815780f973
shaimeur <haimeursaad@gmail.com>
2021-06-28 09:19:14 +0100

    add new features

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,78 @@
 <template>
-  <!-- <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div> -->
-  <router-view />
+  <div></div>
+
+  <!-- <div class="container">
+    <div class="img" :style="`background-image: url('${url}');`"></div>
+    <div class="content"></div> -->
+  <!-- <h1>{{ title }} at {{ date }}</h1> -->
+  <!-- <div class="corps"> -->
+  <!-- <p class="description">{{ description }}</p> -->
+  <!-- </div> -->
+  <!-- </div> -->
+
+  <router-view></router-view>
 </template>
 
+<script>
+import axios from "axios";
+export default {
+  name: "HelloNasa",
+
+  data() {
+    return {
+      date: "-",
+      title: "-",
+      url: "",
+      description: "",
+    };
+  },
+
+  mounted() {
+    const apiUrl =
+      "https://api.nasa.gov/planetary/apod?api_key=vANhpstA65mOYxlGFQaU8zYGyLlhSIsUdiVItnxT";
+    axios
+      .get(apiUrl)
+      .then((res) => {
+        // console.log("res", res);
+        this.title = res.data.title;
+        this.date = res.data.date;
+        this.url = res.data.url;
+        this.description = res.data.explanation;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
+</script>
+
 <style lang="scss">
-body {
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+.container {
+  position: relative;
   width: 100vw;
   height: 100vh;
 }
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.img {
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  background-size: cover;
+  // background-position: center;
+  // background: no-repeat;
 }
 
-#nav {
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.content {
+  position: absolute;
+  top: 50%;
+  left: 80px;
+  transform: translateY(-50%);
+  background-color: aliceblue;
+  width: 25%;
+  height: 70%;
 }
 </style>
